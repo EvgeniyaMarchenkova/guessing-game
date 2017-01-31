b7e5afae569d692f6d02fbe7135f98eb6616d888
Evgeniya Marchenkova <evgeniova@gmail.com>
2017-01-31 12:57:25 +0300

    quessing-game/ all methods implemented but only 6 test passed(

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.secretNumber = null;
+        this.arr = [];
+    }
     setRange(min, max) {
-
+        for ( var i = min; i <= max; i++) {
+            this.arr.push(i);
+        }
     }
-
     guess() {
-
-    }
-
+        if (this.secretNumber = this.arr[0]  ) {
+            return this.arr[0];
+        }
+        if (this.secretNumber = this.arr[this.arr.length-1]) {
+            return this.arr[this.arr.length-1];
+        }
+        if (this.secretNumber = this.arr[1]  ) {
+            return this.arr[1];
+        }
+        return;
+}
     lower() {
-
+        this.arr = this.arr.slice(0, Math.ceil(this.arr.length/2));
     }
 
     greater() {
-
+        this.arr = this.arr.slice( Math.ceil(this.arr.length/2), this.arr.length);
     }
 }
 
